d19ae4946caf0b75f3b45dab08863aacc7f0a23d
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-04-21 16:00:21 -0500

    start data manipulation

--- analyse.py
+++ analyse.py
@@ -16,32 +16,64 @@ def read(filename):
     if(words[4] == "PWR_fabrication" and words[5] == "PWR_Upu_stored" ):
       Pu_storage.append( [int(words[0]),float(words[6])] )
 
+
   return blanket,Pu_storage
 
 def read_data():
   case = [ 1, 2, 3 ]
   method = [ '', '_M' ]
   pu = [ 8,9,10,11,12 ]
-  u  = [ 5,8 ]
+  u  = [ 8 ]
+  pu_contrib = []
+  u_contrib = []
   for case_ in case:
+    
     for method_ in method:
-
+      pu_contrib_ =[]
+      u_contrib_ =[]
       if (case_ == 1 ):
         filename = 'EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'pu' + str(9)
-        read(filename)
+        tmp = read(filename)
+        pu_contrib_.append(tmp)
+        
         filename = 'EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'u' + str(8)
-        read(filename)
+        tmp = read(filename)
+        u_contrib_.append(tmp)
+
       else :
         for pu_ in pu:
           filename = 'EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'pu' + str(pu_)
-          read(filename)
+          tmp = read(filename)
+          pu_contrib_.append(tmp)
         for u_ in u:
           filename = 'EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'u' + str(u_)
-          read(filename)
+          tmp = read(filename)
+          u_contrib_.append(tmp)
+      pu_contrib.append(pu_contrib_)
+      u_contrib.append(u_contrib_)
+  return pu_contrib,u_contrib
 
 
 def main():
-  read_data()
+  pu_contrib, u_contrib = read_data()
+
+  for case in pu_contrib:
+    stg_total_pu = []
+    bkt_total_pu = []
+    for i in range(len(case[0][0])):
+      stg_total_pu.append( [case[0][0][i][0],0] )
+      bkt_total_pu.append( [case[0][0][i][0],0] )
+    for pu in case:
+      for i in range(len(pu[0])):
+        stg_total_pu[i][1] += pu[0][i][1]
+        bkt_total_pu[i][1] += pu[1][i][1]
+    for pu in case:
+      for i in range(len(pu[0])):
+        pu[0][i][1] *= 1/stg_total_pu[i][1]
+        pu[1][i][1] *= 1/bkt_total_pu[i][1]
+
+    print(stg_total_pu)
+
 
 
 main()
