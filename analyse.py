e1afdf4af57f31457b6b3811e1c930d610e53446
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-05-02 17:16:31 -0500

    first commit...

--- analyse.py
+++ analyse.py
@@ -4,22 +4,29 @@ import numpy as np
 def read(filename):
   my_file = open(filename, 'r')
   data = my_file.readlines()
-  blanket = []
-  Pu_storage = []
+  driver_E1 = []
+  driver_E2 = []
+  blt_nat = []
+  blt_E4 = []
   
   for line in data:
     words = line.split()
-    if(words[4] == "PWR_fabrication" and words[5] == "FBR_blanket_Upu_stored" ):
-      blanket.append( [int(words[0]),float(words[6])] )
-    if(words[4] == "PWR_fabrication" and words[5] == "PWR_Upu_stored" ):
-      Pu_storage.append( [int(words[0]),float(words[6])] )
+    if(words[4] == "FBR_driver_fabrication" and words[5] == "E1_second_stored" ):
+      driver_E1.append( [int(words[0]),float(words[6])] )
+    if(words[4] == "FBR_driver_fabrication" and words[5] == "E2_second_stored" ):
+      driver_E2.append( [int(words[0]),float(words[6])] )
+    if(words[4] == "FBR_blanket_fabrication" and words[5] == "natl_u" ):
+      blt_nat.append( [int(words[0]),float(words[6])] )
+    if(words[4] == "FBR_blanket_fabrication" and words[5] == "E4_stored" ):
+      blt_E4.append( [int(words[0]),float(words[6])] )
 
-
-  return [Pu_storage,blanket]
+  print(filename,": ", len(driver_E1), " ", len(driver_E2), " ", len(blt_nat), " ", len(blt_E4)) 
+  return [driver_E1,driver_E2,blt_nat,blt_E4]
 
 def read_data():
   case = [ 1, 2, 3 ]
-  method = [ '', '_M' ]
+#  method = [ '', '_M' ]
+  method = [ '_M' ]
   pu = [ 8,9,10,11,12 ]
   u  = [ 8 ]
   pu_contrib = []
@@ -30,22 +37,22 @@ def read_data():
       pu_contrib_ =[]
       u_contrib_ =[]
       if (case_ == 1 ):
-        filename = 'data/EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'pu' + str(9)
-        tmp1,tmp2 = read(filename)
+        filename = 'data/EG29_Case.2.' + str(case_) + method_ + '.trans.' + 'pu' + str(9)
+        tmp1,tmp2,tmp3,tmp4 = read(filename)
         pu_contrib_.append([tmp1,tmp2])
         
-        filename = 'data/EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'u' + str(8)
-        tmp = read(filename)
+        filename = 'data/EG29_Case.2.' + str(case_) + method_ + '.trans.' + 'u' + str(8)
+        tmp1,tmp2,tmp3,tmp4 = read(filename)
         u_contrib_.append([tmp1,tmp2])
 
       else :
         for pu_ in pu:
-          filename = 'data/EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'pu' + str(pu_)
-          tmp1,tmp2 = read(filename)
+          filename = 'data/EG29_Case.2.' + str(case_) + method_ + '.trans.' + 'pu' + str(pu_)
+          tmp1,tmp2,tmp3,tmp4 = read(filename)
           pu_contrib_.append([tmp1,tmp2])
         for u_ in u:
-          filename = 'data/EG29_Case.1.' + str(case_) + method_ + '.trans.' + 'u' + str(u_)
-          tmp1,tmp2 = read(filename)
+          filename = 'data/EG29_Case.2.' + str(case_) + method_ + '.trans.' + 'u' + str(u_)
+          tmp1,tmp2,tmp3,tmp4 = read(filename)
           u_contrib_.append([tmp1,tmp2])
       pu_contrib.append(pu_contrib_)
       u_contrib.append(u_contrib_)
@@ -55,51 +62,52 @@ def read_data():
 def main():
   pu_contrib, u_contrib = read_data()
 
-  stg_pu_contrib_relativ = []
-  bkt_pu_contrib_relativ = []
-  stg_total_pu = []
-  bkt_total_pu = []
+  drv_E1_pu_contrib_relativ = []
+  drv_E2_pu_contrib_relativ = []
+  drv_E1_total_pu = []
+  drv_E2_total_pu = []
 
   for case_ in pu_contrib:
-    stg_total_pu_ = []
-    bkt_total_pu_ = []
-    stg_pu_contrib_relativ_ = []
-    bkt_pu_contrib_relativ_ = []
+    drv_E1_total_pu_ = []
+    drv_E2_total_pu_ = []
+    drv_E1_pu_contrib_relativ_ = []
+    drv_E2_pu_contrib_relativ_ = []
 
     for i in range(len(case_[0][0])):
-      stg_total_pu_.append( [case_[0][0][i][0],0] )
-      bkt_total_pu_.append( [case_[0][0][i][0],0] )
+      drv_E1_total_pu_.append( [case_[0][0][i][0],0] )
+      drv_E2_total_pu_.append( [case_[0][0][i][0],0] )
 
     for pu in case_:
       for i in range(len(pu[0])):
-        stg_total_pu_[i][1] += pu[0][i][1]
-        bkt_total_pu_[i][1] += pu[1][i][1]
+        drv_E1_total_pu_[i][1] += pu[0][i][1]
+        drv_E2_total_pu_[i][1] += pu[1][i][1]
 
     for pu in case_:
       for i in range(len(pu[0])):
-        pu[0][i][1] *= 1/stg_total_pu_[i][1]
-        pu[1][i][1] *= 1/bkt_total_pu_[i][1]
-      stg_pu_contrib_relativ_.append(pu[0])
-      bkt_pu_contrib_relativ_.append(pu[1])
+        pu[0][i][1] *= 1/drv_E1_total_pu_[i][1]
+        pu[1][i][1] *= 1/drv_E2_total_pu_[i][1]
+      drv_E1_pu_contrib_relativ_.append(pu[0])
+      drv_E2_pu_contrib_relativ_.append(pu[1])
 
-    stg_total_pu___ = np.copy(stg_total_pu_)
-    bkt_total_pu___ = np.copy(bkt_total_pu_)
+    drv_E1_total_pu___ = np.copy(drv_E1_total_pu_)
+    drv_E2_total_pu___ = np.copy(drv_E2_total_pu_)
     for i in range(len(case_[0][0])):
-      stg_total_pu_[i][1] *= 0.001/0.9*0.3/1.5#(stg_total_pu___[i][1]+bkt_total_pu___[i][1])
-      bkt_total_pu_[i][1] *= 0.001/0.9*0.3/1.5#(stg_total_pu___[i][1]+bkt_total_pu___[i][1])
+      drv_E1_total_pu_[i][1] *= 0.001/0.36*0.4/1.5#(drv_E1_total_pu___[i][1]+drv_E2_total_pu___[i][1])
+      drv_E2_total_pu_[i][1] *= 0.001/0.36*0.4/1.5#(drv_E1_total_pu___[i][1]+drv_E2_total_pu___[i][1])
 
-    stg_total_pu.append(stg_total_pu_)
-    bkt_total_pu.append(bkt_total_pu_)
-    stg_pu_contrib_relativ.append(stg_pu_contrib_relativ_)
-    bkt_pu_contrib_relativ.append(bkt_pu_contrib_relativ_)
+    drv_E1_total_pu.append(drv_E1_total_pu_)
+    drv_E2_total_pu.append(drv_E2_total_pu_)
+    drv_E1_pu_contrib_relativ.append(drv_E1_pu_contrib_relativ_)
+    drv_E2_pu_contrib_relativ.append(drv_E2_pu_contrib_relativ_)
 
 
   case = [ 1, 2, 3 ]
-  method = [ 'W', 'M' ]
+#  method = [ 'W', 'M' ]
+  method = [ 'M' ]
   pu = [ 8,9,10,11,12 ]
   u  = [ 8 ]
 
-  file = open("stg_output_pu.dat","w")
+  file = open("drv_E1_output_pu.dat","w")
 
   name = "Time "
   file.write(name)
@@ -116,19 +124,25 @@ def main():
           file.write(name)
   file.write("\n")
 
-  for i in range(len(stg_total_pu[0])):
-    file.write( str(stg_total_pu[0][i][0]/12.) )
+  for i in range(len(drv_E1_total_pu[0])):
+    file.write( str(drv_E1_total_pu[0][i][0]/12.) )
     file.write(" ")
-    for j in range(len(stg_total_pu)):
-      file.write( str(stg_total_pu[j][i][1]) )
+    for j in range(len(drv_E1_total_pu)):
+      if ( len(drv_E1_total_pu[j]) < i+1 ):
+        file.write(str(0))
+      else:
+        file.write( str(drv_E1_total_pu[j][i][1]) )
       file.write(" ")
-      for k in range(len(stg_pu_contrib_relativ[j])):
-        file.write( str(stg_pu_contrib_relativ[j][k][i][1]) )
+      for k in range(len(drv_E1_pu_contrib_relativ[j])):
+        if ( len(drv_E1_pu_contrib_relativ[j][k]) < i+1 ):
+          file.write(str(0))
+        else:
+          file.write( str(drv_E1_pu_contrib_relativ[j][k][i][1]) )
         file.write(" ")
     file.write("\n")
   file.close()
 
-  file = open("bkt_output_pu.dat","w")
+  file = open("drv_E2_output_pu.dat","w")
   
   name = "Time "
   file.write(name)
@@ -145,14 +159,20 @@ def main():
           file.write(name)
   file.write("\n")
 
-  for i in range(len(bkt_total_pu[0])):
-    file.write( str(bkt_total_pu[0][i][0]/12.) )
+  for i in range(len(drv_E2_total_pu[0])):
+    file.write( str(drv_E2_total_pu[0][i][0]/12.) )
     file.write(" ")
-    for j in range(len(bkt_total_pu)):
-      file.write( str(bkt_total_pu[j][i][1]) )
+    for j in range(len(drv_E2_total_pu)):
+      if ( len(drv_E2_total_pu[j]) < i+1 ):
+        file.write(str(0))
+      else:
+        file.write( str(drv_E2_total_pu[j][i][1]) )
       file.write(" ")
-      for k in range(len(bkt_pu_contrib_relativ[j])):
-        file.write( str(bkt_pu_contrib_relativ[j][k][i][1]) )
+      for k in range(len(drv_E2_pu_contrib_relativ[j])):
+        if ( len(drv_E2_pu_contrib_relativ[j][k]) < i+1 ):
+          file.write(str(0))
+        else:
+          file.write( str(drv_E2_pu_contrib_relativ[j][k][i][1]) )
         file.write(" ")
     file.write("\n")
   file.close()
