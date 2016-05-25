7719d1f385443e279956a66f27c6a55412ff57ca
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-05-24 23:16:10 -0500

    update

--- analyse.py
+++ analyse.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 import numpy as np
 
-def read(filename, out_fac, in_fac, commod):
+def readfile_transaction(filename, out_fac, in_fac, commod):
   my_file = open(filename, 'r')
   data = my_file.readlines()
   data_row = []
-
   for line in data:
     words = line.split()
     if( ( words[2] == out_fac or out_fac == "" )
         and (words[4] == in_fac or in_fac == "" )
         and (words[5] == commod or commod == "" ) ):
       data_row.append( [int(words[0]),float(words[6])] )
-
   return data_row
 
-def read_data(file_root, out_fac, in_fac, comods, method):
+
+def readfile_inv(filename):
+  my_file = open(filename, 'r')
+  data = my_file.readlines()
+  data_row = []
+  for line in data:
+    words = line.split()
+    data_row.append( [int(words[0]),float(words[1])] )
+  return data_row
+
+def read_transaction(file_root, out_fac, in_fac, comods, method):
   case = [ 1, 2, 3 ]
-#  case = [ 3 ]
-#  method = [ '_M' ]
   pu = [ 8,9,10,11,12 ]
   u  = [ 8 ]
   pu_contrib = []
@@ -30,30 +36,61 @@ def read_data(file_root, out_fac, in_fac, comods, method):
       u_contrib_ = []
       if ( case_ == 1 ):
         filename = file_root + str(case_) + method_ + '.trans.' + 'pu' + str(9)
-        pu_contrib_.append([read(filename, out_fac, in_fac, comods)])
+        pu_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
         filename = file_root + str(case_) + method_ + '.trans.' + 'u' + str(8)
-        u_contrib_.append([read(filename, out_fac, in_fac, comods)])
+        u_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
       else :
         for pu_ in pu:
           filename = file_root + str(case_) + method_ + '.trans.' + 'pu' + str(pu_)
-          pu_contrib_.append([read(filename, out_fac, in_fac, comods)])
+          pu_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
         for u_ in u:
           filename = file_root + str(case_) + method_ + '.trans.' + 'u' + str(u_)
-          u_contrib_.append([read(filename, out_fac, in_fac, comods)])
+          u_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
       filename = file_root + str(case_) + method_ + '.trans.' + 'pu'
-      pu_contrib_.append([read(filename, out_fac, in_fac, comods)])
+      pu_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
       filename = file_root + str(case_) + method_ + '.trans.' + 'u'
-      u_contrib_.append([read(filename, out_fac, in_fac, comods)])
+      u_contrib_.append([readfile_transaction(filename, out_fac, in_fac, comods)])
       pu_contrib.append(pu_contrib_)
       u_contrib.append(u_contrib_)
   return pu_contrib, u_contrib
 
 
+def read_inv(file_root, method):
+  case = [ 1, 2, 3 ]
+  pu = [ 8,9,10,11,12 ]
+  u  = [ 8 ]
+  pu_contrib = []
+  u_contrib = []
+  
+  for case_ in case:
+    for method_ in method:
+      pu_contrib_ = []
+      u_contrib_ = []
+      if ( case_ == 1 ):
+        filename = file_root + str(case_) + method_ + '.inv.' + 'pu' + str(9)
+        pu_contrib_.append([readfile_inv(filename)])
+        filename = file_root + str(case_) + method_ + '.inv.' + 'u' + str(8)
+        u_contrib_.append([readfile_inv(filename)])
+      else :
+        for pu_ in pu:
+          filename = file_root + str(case_) + method_ + '.inv.' + 'pu' + str(pu_)
+          pu_contrib_.append([readfile_inv(filename)])
+        for u_ in u:
+          filename = file_root + str(case_) + method_ + '.inv.' + 'u' + str(u_)
+          u_contrib_.append([readfile_inv(filename)])
+      filename = file_root + str(case_) + method_ + '.inv.' + 'pu'
+      pu_contrib_.append([readfile_inv(filename)])
+      filename = file_root + str(case_) + method_ + '.inv.' + 'u'
+      u_contrib_.append([readfile_inv(filename)])
+      pu_contrib.append(pu_contrib_)
+      u_contrib.append(u_contrib_)
+  return pu_contrib, u_contrib
+
+
+
 def print_data(nuclide,filename,total,isotopic, method):
   
   case = [ 1, 2, 3 ]
-#  case = [ 3 ]
-#  method = [ 'M' ]
   pu = [ 8,9,10,11,12 ]
   u  = [ 8 ]
   file = open(filename,"w")
@@ -232,16 +269,15 @@ def main():
 
   method_r = [ '', '_M' ]
   method = [ 'W', 'M' ]
-  build_contrib('E3_second', read_data('data/EG29_Case.1.', '', 'PWR_fabrication', 'E3_second_stored', method_r ), 0.001/0.9*0.3/1.5, method )
-  build_contrib('J1_second', read_data('data/EG29_Case.1.', '', 'PWR_fabrication', 'J1_second_stored', method_r ), 0.001/0.9*0.3/1.5, method )
-  build_contrib('J1_prime', read_data('data/eg29_case.1.', 'PWR_separation', '', 'J1_prime', method_r ), 0.001/0.9*0.3/1.5, method )
+  build_contrib('E3_second', read_transaction('data/EG29_Case.1.', '', 'PWR_fabrication', 'E3_second_stored', method_r ), 0.001/0.9*0.3/1.5, method )
+  build_contrib('J1_second', read_transaction('data/EG29_Case.1.', '', 'PWR_fabrication', 'J1_second_stored', method_r ), 0.001/0.9*0.3/1.5, method )
+  build_contrib('J1_prime', read_transaction('data/eg29_case.1.', 'PWR_separation', '', 'J1_prime', method_r ), 0.001/0.9*0.3/1.5, method )
 
   method_r = [ '', '_M', '_MLP', '_MLP-STD', '_MLP-STD-2'  ]
   method = [ 'W', 'M', 'MLP', 'MLP-STD', 'MLP-STD-2' ]
-  build_contrib('MOX_fuel', read_data('data/EG29_Case.1.', 'PWR_fabrication', '', 'PWR_MOX_new', method_r ), 0.001/0.9*0.3/1.5, method )
+  build_contrib('MOX_fuel', read_transaction('data/EG29_Case.1.', 'PWR_fabrication', '', 'PWR_MOX_new', method_r ), 0.001/0.9*0.3/1.5, method )
   
-  build_contrib('LWR_OUT', read_data('data/EG29_Case.1.', '', 'PWR_cooling', 'PWR_MOX_spent', method_r ), 0.001/0.9*0.3/1.5, method )
-#/6.58063000
-  #  build_contrib('test', read_data('data/EG29_Case.1.', '', 'PWR_fabrication', 'J1_second_stored' ), 1 )
+  build_contrib('LWR_OUT', read_transaction('data/EG29_Case.1.', '', 'PWR_cooling', 'PWR_MOX_spent', method_r ), 0.001/0.9*0.3/1.5, method )
 
+  build_contrib('J1_STR', read_inv('data/EG29_Case.1.', method_r ), 0.001/0.9*0.3/1.5, method )
 main()
